ca9af873d4f23fce25fa3e9f7a104f25479dcfcb
k-fujisawa <kouki.fujisawa@tnsgrp.com>
2022-03-21 15:45:49 +0900

    Chore: 環境変数指定(AWS Amplify、APIGateway、ローカルストレージ関連)

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  ampAuthRegion: string;
+  ampAuthUserPoolId: string;
+  ampAuthUserPoolWebClientId: string;
+  apiBaseUrl: string;
+  localstorageBaseKey: string;
 
-  constructor() { }
+  /** constructor */
+  constructor() {}
 
+  /** lifecycle - init */
   ngOnInit() {
+    this.ampAuthRegion = environment.amplify.Auth.region;
+    this.ampAuthUserPoolId = environment.amplify.Auth.userPoolId;
+    this.ampAuthUserPoolWebClientId = environment.amplify.Auth.userPoolWebClientId;
+    this.apiBaseUrl = environment.apiBaseUrl;
+    this.localstorageBaseKey = environment.localstorageBaseKey;
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,18 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   origin: 'http://localhost:3000',
+  amplify: {
+    Auth: {
+      region: 'xxx',
+      userPoolId: 'xxx',
+      userPoolWebClientId: 'xxx',
+    },
+  },
+  apiBaseUrl: 'xxx',
+  localstorageBaseKey: 'xxx',
 };
 
 /*
