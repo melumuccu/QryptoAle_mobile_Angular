2ce56697dc8d80bfca38f5d2e935446b86ae928c
getset-fujisawa <fujisawa.k@getset.jp>
2022-07-18 16:55:25 +0900

    Feature: pull to refresh

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -16,22 +16,52 @@ export class HomePage implements OnInit {
 
   /** 初期化 */
   ngOnInit() {
-    this.fetchPortfolioProfitRatios();
-  }
-
-  /**
-   * 利益率を取得する
-   */
-  fetchPortfolioProfitRatios() {
-    this.callGetPortfolioProfitRatios().subscribe(
-      result => {
-        console.log('file: home.page.ts => line 21 => ngOnInit => result', result);
-        this.portfolioProfitRatios = result;
+    this.fetchPortfolioProfitRatios().subscribe(
+      response => {
+        this.portfolioProfitRatios = response;
       },
       error => {
+        // TODO エラー表示する
         console.error(error);
       }
     );
+  }
+
+  /**
+   * テーブルのリフレッシュ
+   *
+   * @param event
+   */
+  refresh(event) {
+    this.fetchPortfolioProfitRatios().subscribe(
+      response => {
+        this.portfolioProfitRatios = response;
+        event.target.complete();
+      },
+      error => {
+        // TODO エラー表示する
+        console.error(error);
+      }
+    );
+  }
+
+  /**
+   * 利益率など各種テーブル情報を取得する
+   *
+   * @returns Observable<PortfolioProfitRatio>
+   */
+  fetchPortfolioProfitRatios(): Observable<PortfolioProfitRatio[]> {
+    const ob = new Observable<PortfolioProfitRatio[]>(observer => {
+      this.callGetPortfolioProfitRatios().subscribe(
+        response => {
+          observer.next(response);
+        },
+        error => {
+          observer.error(error);
+        }
+      );
+    });
+    return ob;
   }
 
   /**
