7166a783f26b2f8c53c4fe54b3cc3ebae853d8c7
getset-fujisawa <fujisawa.k@getset.jp>
2022-09-27 11:21:38 +0900

    Test: CalculateService#round

--- src/app/shared/services/calculate.service.spec.ts
+++ src/app/shared/services/calculate.service.spec.ts
@@ -13,4 +13,34 @@ describe('CalculateService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#round 指定桁数(n>0)で切り捨て', () => {
+    const target = service.round(999.949, 1);
+    expect(target).toBe(1000.9);
+  });
+
+  it('#round 指定桁数(n>0)で切り上げ', () => {
+    const target = service.round(1000.005, 2);
+    expect(target).toBe(1000.01);
+  });
+
+  it('#round 指定桁数(n=0)で切り捨て', () => {
+    const target = service.round(999.499, 0);
+    expect(target).toBe(999);
+  });
+
+  it('#round 指定桁数(n=0)で切り上げ', () => {
+    const target = service.round(1000.5, 0);
+    expect(target).toBe(1001);
+  });
+
+  it('#round 指定桁数(n<0)で切り捨て', () => {
+    const target = service.round(994.999, -1);
+    expect(target).toBe(990);
+  });
+
+  it('#round 指定桁数(n<0)で切り上げ', () => {
+    const target = service.round(1050.0, -2);
+    expect(target).toBe(1100);
+  });
 });
--- src/app/shared/services/calculate.service.ts
+++ src/app/shared/services/calculate.service.ts
@@ -6,4 +6,14 @@ import { Injectable } from '@angular/core';
 export class CalculateService {
   /** コンストラクタ */
   constructor() {}
+
+  /**
+   * 与えられた数値を指定桁数で四捨五入する
+   *
+   * @param target 対象数値
+   * @param digit 丸め桁数 ex. 0指定: 1234.567 -> 1235, 1指定: 1234.567 -> 1234.6, -1指定: 1234.567 -> 1230
+   */
+  round(target: number, digit: number): number {
+    return 0;
+  }
 }
