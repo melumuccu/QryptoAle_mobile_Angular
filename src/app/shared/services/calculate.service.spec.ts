dc391ae78338e66c4e0e318fe5a636235763b05d
getset-fujisawa <fujisawa.k@getset.jp>
2022-09-27 20:14:22 +0900

    Feature: CalculateService#round

--- src/app/shared/services/calculate.service.spec.ts
+++ src/app/shared/services/calculate.service.spec.ts
@@ -10,36 +10,36 @@ describe('CalculateService', () => {
     service = TestBed.inject(CalculateService);
   });
 
-  it('should be created', () => {
+  test('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('#round 指定桁数(n>0)で切り捨て', () => {
+  test('#round 指定桁数(n>0)で切り捨て', () => {
     const target = service.round(999.949, 1);
-    expect(target).toBe(1000.9);
+    expect(target).toBe(999.9);
   });
 
-  it('#round 指定桁数(n>0)で切り上げ', () => {
+  test('#round 指定桁数(n>0)で切り上げ', () => {
     const target = service.round(1000.005, 2);
     expect(target).toBe(1000.01);
   });
 
-  it('#round 指定桁数(n=0)で切り捨て', () => {
+  test('#round 指定桁数(n=0)で切り捨て', () => {
     const target = service.round(999.499, 0);
     expect(target).toBe(999);
   });
 
-  it('#round 指定桁数(n=0)で切り上げ', () => {
+  test('#round 指定桁数(n=0)で切り上げ', () => {
     const target = service.round(1000.5, 0);
     expect(target).toBe(1001);
   });
 
-  it('#round 指定桁数(n<0)で切り捨て', () => {
+  test('#round 指定桁数(n<0)で切り捨て', () => {
     const target = service.round(994.999, -1);
     expect(target).toBe(990);
   });
 
-  it('#round 指定桁数(n<0)で切り上げ', () => {
+  test('#round 指定桁数(n<0)で切り上げ', () => {
     const target = service.round(1050.0, -2);
     expect(target).toBe(1100);
   });
--- src/app/shared/services/calculate.service.ts
+++ src/app/shared/services/calculate.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import BigNumber from 'bignumber.js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CalculateService {
+  // TODO このクラスの処理はなるべくバックエンドで処理させたい(フロントでBigNumberを扱うのは重い)
   /** コンストラクタ */
   constructor() {}
 
@@ -11,9 +13,24 @@ export class CalculateService {
    * 与えられた数値を指定桁数で四捨五入する
    *
    * @param target 対象数値
-   * @param digit 丸め桁数 ex. 0指定: 1234.567 -> 1235, 1指定: 1234.567 -> 1234.6, -1指定: 1234.567 -> 1230
+   * @param digit 丸め桁数(整数) ex. 0指定: 1234.567 -> 1235, 1指定: 1234.567 -> 1234.6, -1指定: 1234.567 -> 1230
+   * @return 丸め後の対象数値 | null(エラー)
    */
-  round(target: number, digit: number): number {
-    return 0;
+  round(target: number, digit: number): number | null {
+    if (!Number.isInteger(digit)) {
+      console.error('digit が整数でない。');
+      return null;
+    }
+
+    // prettier-ignore
+    const radixBN = digit >= 0 // digit から基数を得る ex. 1 -> 10, -2 -> 0.01
+    ? new BigNumber(10).pow(digit)
+    : new BigNumber(0.1).pow(digit * -1);
+
+    const x = new BigNumber(target).times(radixBN); // targetに基数を掛ける
+    const y = x.dp(0, BigNumber.ROUND_HALF_UP); // 少数第一位を四捨五入
+    const z = y.div(radixBN); // 基数で割って元の桁数に戻す
+
+    return parseFloat(z.toPrecision());
   }
 }
